397000437e81f30d3286151d113f5778f1b6704d
dawoodriaza <od.riaz@alsalambank.com>
2026-03-19 19:09:35 +0300

    wrote csbService Code

--- projects/concurrency/concurrency/src/main/java/com/example/concurrency/Service/CsvService.java
+++ projects/concurrency/concurrency/src/main/java/com/example/concurrency/Service/CsvService.java
@@ -13,6 +13,37 @@ import java.util.List;
 
 @Service
 public class CsvService {
+    public List<Employee> readCsv(String path) {
+        List<Employee> list = new ArrayList<>();
 
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+
+            String line;
+            br.readLine();
+
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+
+            while ((line = br.readLine()) != null) {
+                String[] data = line.split(",");
+
+                Date joinedDate = sdf.parse(data[2]);
+
+                Employee emp = new Employee(
+                        data[0],
+                        Double.parseDouble(data[1]),
+                        joinedDate,
+                        data[3],
+                        Double.parseDouble(data[4])
+                );
+
+                list.add(emp);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return list;
+    }
 
 }
